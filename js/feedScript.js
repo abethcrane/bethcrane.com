5cf26d1041eb930404db464427aa2bd1eb218865
beth <bethc@cse.unsw.edu.au>
2014-09-30 23:34:02 +1000

    Change twitter feed script to handle emojis better
    Evgeny is great :)

--- js/feedScript.js
+++ js/feedScript.js
@@ -13,7 +13,7 @@ var getFeed = function(feedName, feedUrl, numEntries) {
 		},*/
 		dataType: 'json',
 	});
-	
+
 	serverResponse.fail(function(){
 		alert('Unable to load feed, Incorrect path or invalid feed');
 	});
@@ -24,19 +24,19 @@ var getFeed = function(feedName, feedUrl, numEntries) {
 
 var handleData = function(xml, feedName, numEntries) {
 
-	var re  = null; 
+	var re  = null;
 	var img = null;
 	var i;
-	
+
 	feeds[numFeeds] = {"name": feedName, "data": xml};
-	
+
 	if (feedName == "flickr") {
 		for (i = 0; i < numEntries; i++) {
 			// Remove first paragraph of 'abethcrane posted a photo'
 			feeds[numFeeds]["data"][i]["content"] = feeds[numFeeds]["data"][i]["content"].replace(/<p>.*?<\/p>/m, "");
 		}
 	} else if (feedName == "twitter") {
-		for (i = 0; i < numEntries; i++) {	
+		for (i = 0; i < numEntries; i++) {
 			img = feeds[numFeeds]["data"][i]["content"].match(/pic\.twitter\.com[^<]*/);
 			feeds[numFeeds]["data"][i]["publishedDate"] = feeds[numFeeds]["data"][i]["publishedDate"].replace(/.*,/m, "");
 			feeds[numFeeds]["data"][i]["publishedDate"] = feeds[numFeeds]["data"][i]["publishedDate"].replace(/(:[0-9]{2}):.*/m, "$1");
@@ -56,7 +56,7 @@ var handleData = function(xml, feedName, numEntries) {
 	        feeds[numFeeds]["data"][i]["content"] = feeds[numFeeds]["data"][i]["content"].replace(/<strong>(.|\s)*/m, "");
 	    }
 	}
-	
+
 	numFeeds++;
 	if (numFeeds == totalFeeds) {
 		displayTemplate("#sidebarTemplate", nav);
@@ -70,8 +70,8 @@ var feeds = [];
 
 $(document).ready(function() {
 	getFeed("flickr", "http://api.flickr.com/services/feeds/photos_public.gne?id=105674507@N06", 6);
-	getFeed("twitter", "https://script.googleusercontent.com/macros/echo?user_content_key=z9Mx6ocZ1QHGg8p36O7wQGh7TbDRgHfuLzPOnAKW52oL6f0T9c0c3rT0S_fjjyH31YizzRhFpOxKZ2y6W3Cqe4riepYn4PYJm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHiRoBgmO8xZOmNJiOHh7xiBnWSLqMTAQgQXwe4xiy2vhUK22EN0w71ZQSz5XZEBEnEsHGzMQut4&lib=MY4PUmJiFsBUGlQOwt0PZAIXnViTw_VgZ", 5);
+	getFeed("twitter", "https://script.googleusercontent.com/macros/echo?user_content_key=gOvlL1a7DdxWDC311cAcW1EH0OYToLxbKUbpC4CNob_QqVIKCcJ0uv6pxX9_6b7zwzMViBc4waWYH3Cy2-AUoDpq_xIB2AEUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAgDXbj_tVyWQMr79LWC07Ejb66DUKzGjmLsoERNB3kg0BAiR7PDyyKrbLp64XKMJYdYUgLW9EAN1I3KF5EhPlVgRFelZprtXqd3WMz3xaUx&lib=MJ2RCt7KC2yZa3tT8V4zCPBwaZjxFQg_8", 5);
 	getFeed("github", "https://github.com/abethcrane.atom", 5);
 	getFeed("instagram", "http://widget.stagram.com/rss/n/abethcrane", 6);
-	
+
 });
