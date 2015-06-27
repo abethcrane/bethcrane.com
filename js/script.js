a9ee36eb7a2e6a741f9d1210a91e253c252e8a51
beth <abethcrane@gmail.com>
2015-06-27 12:53:25 -0700

    Update to using a different full page scroll

--- js/script.js
+++ js/script.js
@@ -41,22 +41,45 @@ $(document).ready(function() {
         $('.content').css('height', "100%");
     });
     
-    $(".pages").onepage_scroll({
-    sectionContainer: "div.page",     // sectionContainer accepts any kind of selector in case you don't want to use section
-    easing: "linear",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 750,             // AnimationTime let you define how long each section takes to animate
-    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-    beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-    afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-    keyboard: false,                  // You can activate the keyboard controls
-    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                    // the browser's width is less than 600, the fallback will kick in.
-    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
-    });
+    $('.pages').fullpage({
+		//Navigation
+		menu: false,
+		lockAnchors: false,
+		navigation: false,
+		navigationPosition: 'right',
+		showActiveTooltip: false,
+		slidesNavigation: true,
+		slidesNavPosition: 'bottom',
+
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 700,
+		autoScrolling: true,
+		fitToSection: true,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: false,
+		continuousVertical: false,
+		scrollOverflow: true,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+
+		//Accessibility
+		keyboardScrolling: false,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: true,
+		verticalCentered: true,
+
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.page',
+	});
 });
 
 // Ahttp://stackoverflow.com/a/4673436/4629688
